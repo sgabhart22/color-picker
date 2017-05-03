5a6ffe5544c907f8a434dd1149789836ce19911b
Spencer Gabhart <sgabhart2230@gmail.com>
2017-05-03 11:18:54 -0400

    Basic window display

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,36 @@ use opengl_graphics::{ GlGraphics, OpenGL };
 use std::path::Path;
 
 mod color;
+mod app;
+mod settings;
 
 fn main() {
+    let settings = settings::Settings::new();
+
     let opengl = OpenGL::V2_1;
     let mut window: GlutinWindow =  
         WindowSettings::new("Color Picker",
-            [640, 480])
+            [(settings.wind_size.x as u32), (settings.wind_size.y as u32)])
         .exit_on_esc(true)
         .opengl(opengl)
         .build()
         .unwrap();
     let ref mut gl = GlGraphics::new(opengl);
+
+    let mut app = app::App::new(settings);
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.on_render(&args, gl);
+        }
+
+        if let Some(button) = e.press_args() {
+            app.on_button_press(&button);
+        }
+
+        if let Some(args) = e.mouse_cursor_args() {
+            app.on_mouse_move(&args);
+        }
+    } // while let
 }
